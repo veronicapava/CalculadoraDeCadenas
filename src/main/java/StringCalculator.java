fe51122235711844f021bda353486eb8ddf8f512
veronicapava <veronicapava@gmail.com>
2022-03-19 20:24:55 -0500

    funcion para obtener array de numeros

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringCalculator {
 
     //Una cadena vacia devuelve 0
@@ -8,7 +13,11 @@ public class StringCalculator {
             return 0;
         }
         var deli = encontrarDelimitador(input);
-        System.out.println(deli);
+        var numeros = splitInput(input, deli);
+
+        System.out.println("delimitarod es " + deli);
+        System.out.println("Array de numeros " + numeros);
+
         return 0;
     }
 
@@ -33,9 +42,25 @@ public class StringCalculator {
             String caracterAEvaluar = String.valueOf(input.charAt(i));
             if(caracterAEvaluar.equals(delimitadorTemporal))
                 delimitador += delimitadorTemporal;
+            else
+                break;
         }
 
         return delimitador;
+    }
+
+    public List<Integer> splitInput(String input, String delimitador) throws Exception {
+        try {
+            var values = new ArrayList<>(Arrays.asList(input.split(delimitador)));
+            values.removeIf(item -> item == null || "".equals(item));
+
+            return values.stream()
+                         .map(item -> Integer.parseInt(item))
+                         .collect(Collectors.toList());
+        }catch (Exception e){
+            throw new Exception("No es posible hacer split con el delimitador encontrado");
+        }
+
     }
 
 
