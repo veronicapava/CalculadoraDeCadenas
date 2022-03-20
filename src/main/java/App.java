25a6211f487b81defb0356010944caab9b5ae8b4
veronicapava <veronicapava@gmail.com>
2022-03-19 19:09:19 -0500

    metodo para encontrar delimitador

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        StringCalculator cal = new StringCalculator();
+
+        var result =cal.add("444777");
+        System.out.println(result);
+    }
+}
--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,43 @@
 public class StringCalculator {
 
+    //Una cadena vacia devuelve 0
+
+    public int add(String input) throws Exception {
+
+        if(input.isBlank()) {
+            return 0;
+        }
+        var deli = encontrarDelimitador(input);
+        System.out.println(deli);
+        return 0;
+    }
+
+    public String encontrarDelimitador(String input) throws Exception {
+
+        String delimitador ;
+
+        if(Character.isDigit(input.charAt(0))){
+            if (input.matches("^\\d+(,\\d+)*$"))
+                delimitador = ",";
+            else if(input.matches("^\\d+(\\n\\d+)*$"))
+                delimitador = "\n";
+            else
+                throw new Exception("No es posible encontrar delimitador en la cadena");
+            return delimitador;
+        }
+
+        var delimitadorTemporal = String.valueOf(input.charAt(0));
+        delimitador = delimitadorTemporal;
+
+        for (int i = 1; i < input.length(); i++){
+            String caracterAEvaluar = String.valueOf(input.charAt(i));
+            if(caracterAEvaluar.equals(delimitadorTemporal))
+                delimitador += delimitadorTemporal;
+        }
+
+        return delimitador;
+    }
+
+
+
 }
